13635cd4751096e0e7c33749d1a5920f703fed0f
HarrisonBounds <harrison.bounds777@gmail.com>
2025-05-30 08:51:38 -0500

    refactored dataloader for shards

--- test_train.py
+++ test_train.py
@@ -5,26 +5,38 @@ import json
 import time
 import math
 import os
+import numpy as np
 from torch.distributed import init_process_group, destroy_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
 
 #------------------------------------------------------------------------------------------------------------
+def load_tokens(filename):
+    npt = np.load(filename)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
 class DataLoaderTest:
-    def __init__(self, B, T):
+    def __init__(self, B, T, process_rank, num_processes, split):
         self.B = B
         self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
 
-        #Get data
-        with open('input.txt', 'r') as f:
-            text = f.read()
+        assert split in {'train', 'val'}
 
-        enc = tiktoken.get_encoding("gpt2")
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
+        #Get shard names
+        data_folder = "edu_fineweb10B"
+        shards = os.listdir(data_folder)
 
-        self.current_position = 0
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_folder, s) for s in shards]
+        self.shards = shards
 
-        print("Number of tokens loaded: ", len(self.tokens))
-        print(f"1 epochs = {len(self.tokens) // (B * T)} batches")
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank 
 
 
     def next_batch(self):
@@ -34,14 +46,15 @@ class DataLoaderTest:
         buf = self.tokens[self.current_position :  self.current_position + B*T+1]
         x = (buf[:-1]).view(B, T)
         y = (buf[1:]).view(B, T)
-        self.current_position += B * T
+        self.current_position += B * T * self.num_processes #advancement based on gpu
 
         #Wrap back arounf if next batch is out of bounds
-        if self.current_position + (B * T + 1) > len(self.tokens):
-             self.current_position = 0
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = self.B * self.T * self.process_rank
 
         return x, y
-
 #------------------------------------------------------------------------------------------------------------
 
 #Manual LR scheduler
@@ -61,7 +74,6 @@ def get_lr(it, config, min_lr):
 
     return min_lr + coeff * (config["max_lr"] - min_lr)
     
-
 #------------------------------------------------------------------------------------------------------------
 
 #Distributed Data Parallel (DDP), using multiple GPUs for training
@@ -94,8 +106,13 @@ print("Device: ", device)
 with open('gpt_config.json', 'r') as f:
         hyperparameters = json.load(f)
 
+#Set seed for reproducibility and multiple GPU use
+torch.manual_seed(1449)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1449)
+
 total_batch_size = 524288
-B = 32
+B = 64
 T = 1024
 #gradient accumulation
 grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
@@ -106,22 +123,24 @@ if master_process:
 
 print(f"I am GPU rank {ddp_rank}")
 
-import sys ; sys.exit(0)
-
-train_loader = DataLoaderTest(B=B, T=T)
+train_loader = DataLoaderTest(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="train")
 
 torch.set_float32_matmul_precision("high")
 
-#Get logits
+#Create model
 model = GPT(hyperparameters)
 model.to(device)
 model = torch.compile(model)
+
+if ddp:
+    model = DDP(model, device_ids=[ddp_local_rank])
 
 min_lr = hyperparameters["max_lr"] * 0.1
 
 #Optimizer - GPT3 hyperparamters
 optimizer = torch.optim.AdamW(model.parameters(), lr=hyperparameters["max_lr"], betas=(0.9, 0.95), eps=1e-8, weight_decay=0.1)
 
+print("starting training loop...")
 for step in range(hyperparameters["max_steps"]):
     t0 = time.time()
     optimizer.zero_grad()
@@ -136,7 +155,15 @@ for step in range(hyperparameters["max_steps"]):
 
         loss = loss / grad_accum_steps #scale the loss down and apply mean
         loss_accum += loss.detach() #values instead of tensors
+        if ddp:
+            #Toggle bool to not sync until the last step to reduce overhead
+            model.require_backward_grad_sync = (micro_step == grad_accum_steps - 1)
+
         loss.backward()
+
+    if ddp:
+        #Create avg of loss across GPUs
+        dist.all_reduce(loss_accum, op=dist.ReduceOp.AVG) 
 
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) #gradient norm clipping
 
@@ -150,6 +177,10 @@ for step in range(hyperparameters["max_steps"]):
     t1 = time.time()
     dt = t1-t0
 
-    tps = (train_loader.B * train_loader.T * grad_accum_steps) / dt
+    tps = (train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size) / dt
 
-    print(f"Step: {step+1}, Loss: {loss.item()}, LR: {lr}, time: {dt * 1000} ms, tok/sec: {tps}")
+    if master_process:
+        print(f"Step: {step+1}, Loss: {loss_accum.item()}, LR: {lr}, time: {dt * 1000} ms, tok/sec: {tps}")
+
+if ddp:
+    destroy_process_group()
